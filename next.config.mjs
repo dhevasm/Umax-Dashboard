38c5f3e13639af7e20215737ceb129202a803599
dhevasm <dhevasm2019@gmail.com>
2024-07-17 09:07:22 +0700

    test config

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-// next.config.js
-module.exports = {
-    async rewrites() {
-        return [
-          {
-            source: '/api/:path*',
-            destination: 'https://umaxxxxx-1-r8435045.deta.app',
-          },
-        ]
-      },
-  };
+const nextConfig = {
+    beforeFiles: [
+        {
+        source: '/backend/:path*',
+        destination: 'https://umaxxxxx-1-r8435045.deta.app/:path*',
+        },
+    ],
+};
 
 export default nextConfig;
